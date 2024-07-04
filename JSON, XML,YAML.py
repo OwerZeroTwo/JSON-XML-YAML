ea1dd1da513a0a7a1ba35f8f114acb148c17f086
OwerZeroTwo <161704626+OwerZeroTwo@users.noreply.github.com>
2024-07-04 19:14:29 +0300

    Add files via upload

--- JSON, XML,YAML.py
+++ JSON, XML,YAML.py
@@ -1,29 +1,27 @@
 import json
 
 def employees_rewrite(sort_type):
-    try:
-        with open('employees.json', 'r') as file:
-            data = json.load(file)
+    with open('employees.json', 'r') as file:
+        data = json.load(file)
 
-        if sort_type not in ('firstname', 'lastname', 'department', 'salary'):
-            raise ValueError('Bad key for sorting')
+    sort_type = sort_type.lower()
 
-        key_to_sort = sort_type.lower()
-        if key_to_sort == 'salary':
-            key_to_sort = 'salary' if isinstance(data['employees'][0]['salary'], int) else 'salary_str'
-        else:
-            key_to_sort = key_to_sort.replace('name', 'Name')
+    for employee in data['employees']:
+        if sort_type in [key.lower() for key in employee.keys()]:
+            break
+    else:
+        raise ValueError('Bad key for sorting')
 
-        sorted_data = sorted(data['employees'], key=lambda x: x[key_to_sort])
+    data['employees'].sort(key=lambda x: x.get(sort_type.capitalize(), x.get(sort_type.upper(), x.get(sort_type, ''))), reverse=sort_type.isdigit())
 
-        for i, employee in enumerate(sorted_data):
-            employee['id'] = i
+    with open(f'employees_{sort_type}_sorted.json', 'w') as file:
+        json.dump(data, file, indent=4)
 
-        with open(f'employees_{sort_type}_sorted.json', 'w') as file:
-            json.dump(data, file, indent=4)
+    with open(f'employees_{sort_type}_sorted_2.json', 'w') as file:
+        json.dump(data, file, indent=4)
 
-    except Exception as e:
-        print(f'Error: {e}')
+    with open(f'employees_{sort_type}_sorted_3.json', 'w') as file:
+        json.dump(data, file, indent=4)
 
 # Test the function
 employees_rewrite('lastname')
